32e49729d7342ff595cd4a950a0046d41ac65c77
leesh9069 <shlee9069@naver.com>
2021-08-05 16:47:03 +0900

    8/5 LSH commit 5

--- accountapp/views.py
+++ accountapp/views.py
@@ -38,8 +38,10 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.pk})
 
 class AccountDetailView(DetailView):
     model = User
@@ -54,8 +56,6 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
-
-
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
